63926ed7b70b3c6ef98f550eaa7425f1ab37306f
Khem Raj <62122006+Khemaryal@users.noreply.github.com>
2020-07-04 09:28:41 +0200

    Mainly change on product page and contact page
    Mainly change on product page and included style in contact page

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
-const productListLink = "https://spreadsheets.google.com/feeds/list/1MwvKQ_TTl93LEKroQ0Rxnk5wMAjeXRDDO2SZVGzpgho/od6/public/values?alt=json ";
+const productListLink =
+  "https://spreadsheets.google.com/feeds/list/1MwvKQ_TTl93LEKroQ0Rxnk5wMAjeXRDDO2SZVGzpgho/od6/public/values?alt=json ";
 
+function loadJSON(link) {
+  fetch(productListLink)
+    .then((e) => e.json())
+    .then((data) => data.feed.entry.forEach(displayProducts));
+}
 
+const template = document.querySelector("template").content;
 
+function displayProducts(dairy) {
+  const clone = template.cloneNode("true");
+  clone.querySelector("img").setAttribute("src", dairy.gsx$images.$t);
+  clone.querySelector("#animalproduct").textContent = dairy.gsx$productions.$t;
+  clone.querySelector("#value").textContent = dairy.gsx$price.$t;
+
+  if (dairy.gsx$items.$t === "Milk") {
+    document.querySelector("#products").appendChild(clone);
+  }
+}
+
+loadJSON(productListLink);
 
 function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.style.display == "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+  var x = document.getElementById("menu");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
