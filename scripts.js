1b687a0a8f203d3010cac7bbed245977bee9863a
Khem Raj <62122006+Khemaryal@users.noreply.github.com>
2020-07-04 02:45:49 +0200

    Website of Gaunmilks
    This is the first verson of gaunmilks website.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const productListLink = "https://spreadsheets.google.com/feeds/list/1MwvKQ_TTl93LEKroQ0Rxnk5wMAjeXRDDO2SZVGzpgho/od6/public/values?alt=json ";
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
